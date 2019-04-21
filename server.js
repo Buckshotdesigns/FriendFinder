6836a796cf615d595947636d4735b5b4a9389ca1
bryanbuckus <bryanbuckus@gmail.com>
2019-04-21 14:43:26 -0400

    adding routing

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 var express = require("express");
 var app = express();
+var path = require("path");
 var PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
 
 
 app.listen(PORT, function() {

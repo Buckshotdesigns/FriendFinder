7ce50f3f29499f6ead97a0e55bebde6c0fa0b8d2
bryanbuckus <bryanbuckus@gmail.com>
2019-04-22 22:33:08 -0400

    getting api routes to work

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,4 +1,5 @@
 var path = require("path");
+var friendList = require('../data/friends.js');
 
 module.exports = function(app) {
 
@@ -7,7 +8,7 @@ module.exports = function(app) {
       });
     
       app.post("/api/friends", function(req, res) {
-        res.json();
+        res.json(friendList);
       });
 
 };
